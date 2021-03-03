8b3c99052b9bd644fe6c818e67461f7929a50789
Glen Lewis <glenlewis05@gmail.com>
2021-03-03 14:57:45 -0800

    Implements code that creates a date variable
    The code concatenates the 'year', 'month' variables then adds a 'day piece' (01), to create a class(date) variable.
    
    The code then reorganizes the dataframe and adds comments code clairifcation.

--- 03_code/library_load_initial_wag.R
+++ 03_code/library_load_initial_wag.R
@@ -6,11 +6,20 @@ library(vtable)
 library(haven)
 library(lubridate)
 
-# data load and review ----
+# data load and intial wrangling ----
 
 base_emp_data <- read_dta('01_raw_data/cps_00003.dta')
 
 vt(base_emp_data, lush = TRUE)
+
+# filters out 'whyunemp == 0' which is used to catalogs those in the military or 
+# government service.  Selects the desired variables. It further filters the data
+# keeping only the data for people in retail occupations (47xx), 'whyunemp == 1,2,3' 
+# which correspond to unemployment due to layoffs, ending of temporary work, and 
+# lose of work.  It converts the variables 'occ2010' and 'whyunemp' from numeric 
+# to class(factor).
+
+# wip = work in progress = this is the dataframe that is manipulated 
 
 wip_emp_data <- base_emp_data %>% filter(!(whyunemp == 0)) %>% 
   select('year', 'month', 'serial', 'pernum', 'occ2010', 'whyunemp') %>% 
@@ -18,6 +27,15 @@ wip_emp_data <- base_emp_data %>% filter(!(whyunemp == 0)) %>%
   filter(whyunemp %in% c(1,2,3)) %>% 
   class_conversion(c('occ' , 'why'), as.factor) %>% 
   class_conversion('month', as.numeric)
+
+# concatenates year, month variables then adds a day piece, to create a class(date)
+# variable.
+
+wip_emp_data <- wip_emp_data %>% 
+  mutate(survey_dates = ymd(paste0(year, '-', month, '-01'))) %>% 
+
+# reoders the variables 
+wip_emp_data <- wip_emp_data[c(7, 1, 2, 3, 5, 6, 4)]
 
 vt(wip_emp_data, lush = TRUE)
 
