fedf00b7ee12e4dc7d3b10ec837d7a73873bac7c
Glen Lewis <glenlewis05@gmail.com>
2021-03-10 09:56:15 -0800

    Implements analysis code
    This commits contains the national and regional level DiD of reported layoffs v. states with SAH orders v. survey dates. Also contains visualizations.

--- 03_code/01_library_load_initial_wag.R
+++ 03_code/01_library_load_initial_wag.R
@@ -6,13 +6,12 @@ library(vtable)
 library(haven)
 library(lubridate)
 
-# data load and initial wrangling ----
+# data load ----
 
-base_emp_data <- read_dta('01_raw_data/cps_00004.dta')
+base_emp_data <- read_dta('01_raw_data/cps_00007.dta')
 base_closure_data <- read.csv('02_derived_data/state_lockdown_data.csv')
 
-vt(base_emp_data, lush = TRUE)
-vt(base_closure_data, lush = TRUE)
+# base dataframe wrangling ----
 
 # filters out 'whyunemp == 0' which is used to catalogs those in the military or 
 # government service.  Selects the desired variables. It further filters the data
@@ -23,33 +22,41 @@ vt(base_closure_data, lush = TRUE)
 
 # wip = work in progress = this is the dataframe that is manipulated 
 
+class_conversion <- function(df, x, y){
+  'str_prefix' = x
+  'new_class' = y
+  df %>% mutate(across(starts_with(str_prefix), new_class))
+}
+
 wip_emp_data <- base_emp_data %>% filter(!(whyunemp == 0)) %>% 
-  select('year', 'month', 'serial', 'statefip', 'pernum', 'occ2010', 'whyunemp') %>% 
   filter(occ2010 %in% c(4700, 4720, 4740, 4760)) %>% 
   filter(whyunemp %in% c(1,2,3)) %>% 
-  class_conversion(c('occ' , 'why'), as.factor) %>% 
-  class_conversion('month', as.numeric)
+  class_conversion(c('month', 'durunemp'), as.numeric) %>% 
+  class_conversion(c('occ', 'covidunaw'), as.factor)
 
 # concatenates year, month variables then adds a day piece, to create a class(date)
 # variable.
 
 wip_emp_data <- wip_emp_data %>% 
-  mutate(survey_dates = ymd(paste0(year, '-', month, '-01')))
+  mutate(survey_dates = ymd(paste0(year, '-', month, '-01'))) %>% 
+  select(-'year', -'month')
 
-# converts base_closure_data variable 'lck_dwn_st' from class(charater) to class(date)
+# following code tiddies up the data by converting the factor levels in whyunemp
+# to individual variables 
+
+wip_emp_data <- wip_emp_data %>% group_by(survey_dates, statefip) %>% 
+  mutate(layoff = sum(whyunemp == 1), other_type_loss = sum(whyunemp == 2), 
+  temp_job_end = sum(whyunemp == 3), covidunaw = sum(covidunaw == 2)) %>% 
+  select(-'whyunemp')
+
+# converts base_closure_data variable 'lck_dwn_st' from class(character) to class(date)
 
 wip_state_closures <- base_closure_data %>% 
   mutate(lck_dwn_st = mdy(lck_dwn_st))
 
 # builds a complete dataframe with a left join, based on the variable 'statefip'
 
-wip_emp_closure_data <- wip_emp_data %>% left_join(wip_state_closures, 'statefip')
-
-# reorders the variables 
-
-wip_emp_closure_data <- wip_emp_data[c(8,1,2,3,4,6,7,5)]
-
-vt(wip_emp_data, lush = TRUE)
+base_emp_closure_data <- wip_emp_data %>% left_join(wip_state_closures, 'statefip')
 
 
   
---
+++ 03_code/02_did_wrang_analysis_code.R
@@ -0,0 +1,104 @@
+# DiD wrangling and analysis
+
+# dummy variable build ----
+
+# creating a dummy variable for states that implemented a stay-at-home order (1) and 
+# those states that did not (0)
+
+wip_emp_closure_did <- base_emp_closure_data %>% 
+  mutate(treated = case_when((!is.na(lck_dwn_st) ~ 1), (is.na(lck_dwn_st) ~ 0))) %>% 
+  class_conversion('treated', as.factor)
+    
+# creating a dummy variable for the pre-treatment times and post-treatment times.
+# could have used simpler code but wanted to keep dummy variables in same format.
+
+wip_emp_closure_did <- wip_emp_closure_did %>% 
+  mutate(treatment_after = case_when((survey_dates >= as.Date('2020-03-18') ~ 1), 
+  (survey_dates <= as.Date('2020-03-18') ~ 0))) %>% 
+  class_conversion('treatment_after', as.factor) 
+
+# national DiD modeling----
+
+# the following model looks at the variable 'reason for unemployment' (whyunemp) 
+# against treated and treatment_after variables for all states in the survey to 
+# identify differences between those with stay-at-home orders and those without.
+# With the hypothesis stating that stay-at-home orders resulting from COVID-19 
+# is what affected the health of the retail industry. The following attempts to 
+# assess if the stay-at-home orders did affect unemployment as a sign of industry health.  
+
+national_data <- wip_emp_closure_did %>% 
+  group_by(state, survey_dates) %>% 
+  summarise(lck_dwn_st, layoff, other_type_loss, temp_job_end, covidunaw, treated, 
+  treatment_after) %>% 
+  distinct()
+
+# plots the rate un those unemployed due to being laid off against survey dates.
+# the red line is the date of first lock downs.
+
+ggplot(national_data, aes(x=survey_dates, y=layoff)) + 
+  geom_smooth(se = FALSE) + 
+  geom_smooth(aes(x = survey_dates, y = covidunaw), color = 'black', se = FALSE) +
+  geom_vline(xintercept = as.Date("2020-03-18"), color = 'red') +
+  xlab('Survey Dates') +
+  ylab('Reported Unemployment Rates') +
+  ggtitle('National Layoffs and COVID Related Unemployment verse Survey Dates') 
+
+
+lm_national <- lm(layoff ~ treated + treatment_after + treated:treatment_after, 
+                       data = national_data)
+
+export_summs(lm_national, model.names = 'National DiD Layoff Model')
+
+effect_plot(lm_national, treatment_after, plot.points = TRUE, 
+            x.label = 'Survey Dates', 
+            y.label =  'Reported Layoff Rates per Month',
+            point.color = 'blue2', 
+            main.title = "DiD National Model between Stay-at-Home Order States")
+
+# because the national model compares states with different population sizes, 
+# densities and economies that could impact the modeling, the next model filters the 
+# data set to a group of midwest states (Iowa, Nebraska, Minnesota, Wisconsin, 
+# Illinois, Missouri, Kansas, Colorado, Wyoming) which either had or did not have 
+# stay-at-home orders (Iowa and Nebraska). This model also includes 
+# the variable that specifically asks if unemployment is COVID-19 related. 
+
+# ASSUMPTION: the proximity of the control and treatment states leads to similar 
+# populations densities, economies, etc. 
+
+regional_data <- wip_emp_closure_did %>% 
+  filter(state %in% c('Minnesota', 'Wisconsin', 'Illinois', 'Missouri', 'Kansas',
+  'Colorado', 'Wyoming','Iowa', 'Nebraska')) %>% 
+  group_by(state, survey_dates) %>% 
+  summarise(lck_dwn_st, layoff, other_type_loss, temp_job_end, covidunaw, treated,
+  treatment_after) %>% 
+  distinct()
+
+ggplot(regional_data, aes(x=survey_dates, y=layoff)) + 
+  geom_smooth(se = FALSE) + 
+  geom_smooth(aes(x = survey_dates, y = covidunaw), color = 'black', se = FALSE) +
+  geom_vline(xintercept = as.Date("2020-03-18"), color = 'red') +
+  xlab('Survey Dates') +
+  ylab('Reported Unemployment Rates') +
+  ggtitle('Regional Layoffs and COVID Related Unemployment verse Survey Dates') 
+
+lm_regional <- lm(layoff ~ treated + treatment_after + treated:treatment_after, 
+                            data = regional_data)
+
+export_summs(lm_national, lm_regional, 
+             model.names = c('National DiD Layoff', 'Regional DiD Layoff'))                      
+
+effect_plot(lm_national, treatment_after, plot.points = TRUE, 
+            x.label = 'Survey Dates', 
+            y.label =  'Regional Reported Layoff Rates per Month',
+            point.color = 'blue2', 
+            main.title = "DiD Regional Model between Stay-at-Home Order States")
+
+# ASSUMTPION: As the virus began to spread across the country and local 
+# authorities started implementing individual restrictions - first State 
+# Emergency Declaration was 9 March - retail unemployment 
+# (and thus the health of the retail sector) started to go downhill prior to 
+# stay at home orders. Since we are looking for changes in rates of unemployment
+# specifically (layoff) before and after stay-at-home orders filtering duration 
+# employed to 4 weeks and less should allow us to capture initial layoffs that
+# started occurring before stay-at-home orders as a result of individual actions.
+
