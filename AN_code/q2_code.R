bb37387c1558a2c1e926ed27dc0b0dff8c87490b
Andrew Nalundasan <andrewn488@gmail.com>
2021-03-14 22:00:40 -0700

    Commit after working 3/14/2021. Did reorder of industry factor variable in order to make comparisons to Retail industry in export_summs(). Touched up data and maintained ggplot. Began writing up analysis in RMarkdown. To be continued tomorrow with intention of submitting markdown to Glen on Tuesday 3/16.

--- AN_code/q2_code.R
+++ AN_code/q2_code.R
@@ -19,16 +19,13 @@ library(zoo) # dates library with yearmo
 options(scipen=999) # remove scientific notation
 library(data.table)
 library(ggplot2)
+library(stats)
 
 # read in data files
 retail <- read_csv('../AN_raw_data/cps_00002.csv.gz')
 
 # load industry name / code file
 inddf <- read_csv("../AN_raw_data/indnames.csv")
-
-# look at the data
-# write.csv(retail, '../AN_raw_data/retail.csv', row.names = FALSE)
-# write.csv(employment, 'AN_raw_data/employment.csv', row.names = FALSE)
 
 ####### Working with Data ##########
 
@@ -78,48 +75,78 @@ retail_vs_ind_v2 <- retail_vs_ind_v2 %>%
   filter(industry != "NA") %>% 
   mutate(industry = factor(industry))
 
-vtable(retail_vs_ind_v2)
-# ^^ interact these Industry variables *covid
-# ^^ interact covid*time
+# reorder the levels of industry factor for Retail to be on top
+retail_vs_ind_v3 <- retail_vs_ind_v2 %>% 
+  filter(industry != "NA") %>% 
+  mutate(industry = factor(industry, levels = rev(levels(industry))))
 
-# regression models: 
-m1 <- lm(employed ~ Retail, data = retail_vs_ind_v2)
-m2 <- lm(employed ~ Retail + industry*covid, data = retail_vs_ind_v2)
-
-export_summs(m1)
-
-
-######## add weight to survey data 
-retail_vs_ind_v2 <- retail_vs_ind_v2 %>% 
+######## add weight to survey data ##############
+retail_vs_ind_v4 <- retail_vs_ind_v3 %>% 
   filter(WTFINL != "NA") %>% 
   filter(employed != "NA")
 
-
 survey <- 
-  as_survey(retail_vs_ind_v2, weights = c(WTFINL)) 
+  as_survey(retail_vs_ind_v4, weights = c(WTFINL)) 
 
 
-## 
+# make rate for all industries
 industry_rate <- survey %>%
   group_by(YEAR, MONTH, industry) %>%
   summarize(employment_rate = survey_mean(employed, vartype = "ci")) %>%
   pivot_wider(names_from = industry, values_from = c(employment_rate,employment_rate_low,employment_rate_upp))
 
-# merge dataset:
-merged_df <- left_join(retail_vs_ind_v2, industry_rate, by = c("YEAR", "MONTH"))
+# merge datasets:
+merged_df <- left_join(retail_vs_ind_v4, industry_rate, by = c("YEAR", "MONTH"))
 
+# create year_month variable and set type to date
 merged_df <- merged_df %>% 
-  unite(year_month, c("YEAR", "MONTH"), sep = "-")  %>%
+  unite(year_month, c("YEAR", "MONTH"), sep = "-", remove = FALSE)  %>%
   mutate(year_month = (Date = as.yearmon(year_month))) %>%
   mutate(year_month = as.Date(year_month))
 
-vtable(merged_df)
 
-# Plot
+# Employment Rate 
+employment_rate <- survey %>% 
+  group_by(YEAR, MONTH) %>% 
+  summarize(employment_rate = survey_mean(employed))
+
+# merge employment rate into df
+merged_df <- left_join(merged_df, employment_rate, by = c("YEAR", "MONTH")) 
+
+
+########### Industry Analysis ################
+# retail reg
+retail_reg <- lm(employment_rate ~ Retail*covid + covid*time, merged_df)
+
+# arts and stuff reg
+Arts_Ent_Rec_Accom_Food_reg <- lm(employment_rate ~ Arts_Ent_Rec_Accom_Food*covid + covid*time, merged_df)
+
+# construction reg
+construction_reg <- lm(employment_rate ~ Construction*covid + covid*time, merged_df)
+
+# FINC_Ins_Real_Estate reg
+FINC_Ins_Real_Estate_reg <- lm(employment_rate ~ FINC_Ins_Real_Estate*covid + covid*time, merged_df)
+
+# Public_Admin reg
+Public_Admin_reg <- lm(employment_rate ~ Public_Admin*covid + covid*time, merged_df)
+
+# look at the regressions
+export_summs(retail_reg, Arts_Ent_Rec_Accom_Food_reg, construction_reg,
+             FINC_Ins_Real_Estate_reg, Public_Admin_reg, digits = 5,
+             lush = TRUE)
+
+# industry reg:
+#  <- lm_robust(employed ~ industry*covid + covid*time, merged_df)
+industry_reg_z <- lm(employment_rate ~ industry*covid + covid*time, merged_df)
+
+export_summs(industry_reg_z, digits = 5, lush = TRUE)
+
+
+# Plot industries against each other
 industry_plot <- ggplot(merged_df, aes(year_month)) +
   
   geom_line(aes(
-    y = employment_rate_Arts_Ent_Rec_Accom_Food, color = "Arts and stuff")) +
+    y = employment_rate_Arts_Ent_Rec_Accom_Food, color = "Arts-Entertainment-Recreation-Food")) +
   geom_ribbon(aes(ymax = employment_rate_upp_Arts_Ent_Rec_Accom_Food,
                   ymin = employment_rate_low_Arts_Ent_Rec_Accom_Food), alpha=0.2) +
   
@@ -129,7 +156,7 @@ industry_plot <- ggplot(merged_df, aes(year_month)) +
                   ymin = employment_rate_low_Construction), alpha=0.2) +
   
   geom_line(aes(
-    y = employment_rate_FINC_Ins_Real_Estate, color = "Finance Insurance Real Estate")) +
+    y = employment_rate_FINC_Ins_Real_Estate, color = "Finance-Insurance-Real Estate")) +
   geom_ribbon(aes(ymax = employment_rate_upp_FINC_Ins_Real_Estate,
                   ymin = employment_rate_low_FINC_Ins_Real_Estate), alpha=0.2) +
   
@@ -153,133 +180,23 @@ industry_plot <- ggplot(merged_df, aes(year_month)) +
   
   theme(axis.text.x=element_text(angle=60, hjust=1))
 
-print(industry_plot)
-
-
-# Q1:Regression
-## layoff ~ independent variables + other variables
-retail_rate <- survey %>% 
-  group_by(YEAR, MONTH) %>% 
-  summarize(retail_rate = survey_mean(employed))
+# look at the plot
+industry_plot
 
 
 
+##### working code - demeaned approach ######
+# demeaned industries
+# merged_df_dm <- merged_df %>% group_by(industry) %>% 
+#  mutate(retail_dm = Retail - mean(Retail),
+#         Arts_Ent_Rec_Accom_Food_dm = Arts_Ent_Rec_Accom_Food - mean(Arts_Ent_Rec_Accom_Food),
+#         Constructin_dm = Construction - mean(Construction),
+#         FINC_Ins_Real_Estate_dm = FINC_Ins_Real_Estate - mean(FINC_Ins_Real_Estate),
+#         Public_Admin_dm = Public_Admin - mean(Public_Admin))
 
+# fe_dm <- lm(employed ~ retail_dm + Arts_Ent_Rec_Accom_Food_dm + Constructin_dm +
+#              FINC_Ins_Real_Estate_dm + Public_Admin_dm, merged_df_dm)
 
-
-
-# Regression: 
-# WHYUNEMP ~ Retail + Industry A + Industry B + Industry C + YEAR + e
-# I need to tune up this model
-# What other models would be of value?
-
-# pull out Industries -- Retail vs. others
-# difference between mine and JG - she puts "other industry" into "Other" and I break it up to different ones
-retail_vs_sector <- retail_select %>% 
-  mutate(Retail = Industry %between% c(4670,5790),
-         Arts_Ent_Rec_Accom_Food = Industry %between% c(8560, 8690),
-         Construction = Industry == 770,
-         FINC_Ins_Real_Estate = Industry %between% c(6870, 7190),
-         Public_Admin = Industry %between% c(9370, 9590)
-  )
-
-vtable(retail_vs_sector)
-
-
-m1 <- lm(WHYUNEMP ~ Retail + Arts_Ent_Rec_Accom_Food + Construction + FINC_Ins_Real_Estate +
-           Public_Admin, data = retail_vs_sector)
-
-m2 <- lm(Retail ~ YEAR, data = retail_vs_sector)
-
-## need to interact COVID with each industry
-
-
-export_summs(m1, m2)
-
-
-
-
-
-# look at variables and take note of Class
-vtable(retail)
-
-# select necessary variables
-retail_select <- retail %>% select("YEAR", "MONTH", "CPSID", "STATEFIP", "Occupation", "Industry_1990 basis", "Industry", "Industry Last Year")
-vtable(retail_select)
-
-# group by Industry
-# retail_group_by <- retail_select %>% group_by(Industry)
-
-# plot the data to look at raw relationship
-#density <- ggplot(retail_select, aes(x = Industry)) + 
-#  geom_density() + 
-#  labs(x = 'Industry', title = 'Distribution of Industry across Years')
-# density
-
-
-#ggplot(data = retail_group_by) +
-#  geom_histogram(mapping = aes(x = Industry), binwidth = 1000) +
-#  xlab('Industry') +
-#  ggtitle('Distribution of Industry across Years')
-
-# scatterplot by year
-# retail_select %>% 
-#  group_by(YEAR, Industry) %>% 
-#  ggplot(mapping = aes(x = YEAR, y = Industry, color = Industry)) +
-#  geom_point() +
-#  my_theme +
-#  labs(title = "Distribution of Industries across Years",
-#       subtitle = "Industries and Years",
-#       x = "Year",
-#       y = "Industry") +
-#  scale_color_manual(values = c("winter" = "#d6dee1", "spring" = "#d6dee1",
-#                                "summer" = "#002060", "fall" = "#d6dee1")) +
-#  theme(axis.text.x = element_blank(),
-#        axis.ticks.x = element_blank())
-
-
-
-##### Data Viz ###########
-# my_theme <- theme(panel.border = element_blank(),
-#                  panel.background = element_blank(),
-#                  axis.line = element_line(color = "gray"),
-#                  axis.ticks = element_line(color = "gray"),
-#                  plot.title = element_text(color = "#808080", size = 16, face = "bold"),
-#                  plot.subtitle = element_text(color = "#808080", size = 14, face = "plain"),
-#                  axis.text.x = element_text(color = "#808080"),
-#                  axis.text.y = element_text(color = "#808080"),
-#                  axis.title = element_text(color = "#808080", size = 10, face = "plain"),
-#                  legend.title = element_text(color = "#808080", size = 12, face = "plain"),
-#                  legend.text = element_text(color = "#808080", size = 10, face = "plain"),
-#                  plot.caption = element_text(color = "#808080", size = 10, face = "plain")
-#)
-
-
-
-## Mikayla Viz ##
-
-
-#sum of unemployed
-sum_unemp <- wip_emp_data %>%
-  count(survey_dates) %>%
-  filter(survey_dates >= "2019-11-01")
-
-plot_total <- ggplot(data = sum_unemp, aes(survey_dates, n)) +
-  geom_bar(stat = "identity") + 
-  plot_total
-
-sum_by_why <- wip_emp_data %>%
-  count(survey_dates, whyunemp) %>%
-  filter(survey_dates >= "2019-11-01")
-
-plot_by_why <- sum_by_why %>%
-  ggplot(aes(x = survey_dates, y = n, fill = factor(whyunemp)))+
-  scale_fill_discrete(name = "Reason",
-                      labels=c("Laid Off", 
-                               "Temporary Work Concluded", "Loss of Work")) +
-  geom_bar(position = "dodge", stat = "identity") +
-  theme(legend.position="bottom") + 
-  xlab("Month") +
-  ylab("Number of People")
-
-plot_by_why
+# LSDV: 
+# fe_lsdv <- lm(employed ~ Retail + covid + industry, merged_df)
+# linearHypothesis(fe_lsdv, matchCoefs(fe_lsdv, 'industry'))
